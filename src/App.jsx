d6e68826cf98c0c86eac6652811f718cc86d4536
E-Jeff <86261757+E-Jeff@users.noreply.github.com>
2023-12-19 10:39:48 +0100

    Hero section
    Left menu animation done ; hero section in work

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
-  return <div>
-    <section id="Homepage">
-      <Navbar/>
-    </section>
-    {/* <section><a href="#">Hero</a></section> */}
-    <section id="Services">Parallax</section>
-    <section>Services</section>
-    <section id="Portefolio">Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contact">Contact</section>
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portefolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
